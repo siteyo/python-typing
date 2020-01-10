3ff2d4978e8db4c5c1f93150912957983ff184ee
siteyo <phicyanoc@gmail.com>
2020-01-10 22:02:33 +0900

    Add requirements.txt

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # coding: utf-8
 
-from types import Point
+from types import Line, Point
 
-p1: Point = {"x": 10, "y": 5, "label": "S"}
-p2: Point = {"x": 15, "y": 2, "label": "D"}
+p1: Point = {"x": 10, "y": 5, "label": "S"}  # ok
+p2: Point = {"x": 15, "y": 2, "label": "D"}  # ok
+p3: Point = {"x": 20}  # ng
+
+l1: Line = [p1, p2]  # ok
+l2: Line = [1, 2]  # ng
--- types.py
+++ types.py
@@ -1,8 +1,11 @@
 # coding: utf-8
-from typing import TypedDict
+from typing import List, TypedDict
 
 
 class Point(TypedDict):
     x: int
     y: int
     label: str
+
+
+Line = List[Point]
