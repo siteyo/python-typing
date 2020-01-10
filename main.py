40a06195fb575410522902d4d0b67a50e0ea85f8
siteyo <phicyanoc@gmail.com>
2020-01-10 21:15:34 +0900

    Add Point type

---
+++ main.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+from types import Point
+
+p1: Point = {"x": 10, "y": 5, "label": "S"}
+p2: Point = {"x": 15, "y": 2, "label": "D"}
---
+++ types.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+from typing import TypedDict
+
+
+class Point(TypedDict):
+    x: int
+    y: int
+    label: str
